7d78c191f098348cae42f3a15bdb8c73b9e5fb6f
TriddyKay <tristankay93@gmail.com>
2021-11-01 15:10:15 +1100

    fix name and some styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/home.css'
+import {Header} from "semantic-ui-react"
 
 export const Home = () => {
 
@@ -15,8 +16,9 @@ export const Home = () => {
         <img src={'https://res.cloudinary.com/dqsacxogt/image/upload/v1573648191/BlogPhotos/moustache_r5dvaz.png'} alt={''} className={'moustache'}/>
       </div>
       <div className={'teaser'}>
-      "Chriz Chrysler, Panini D Dragon & Trizzy Rascal pick up sporks to take on the world"
+        Chriz Chrysler, Panini D Dragon & Trizzy Rascal pick up sporks to take on the world...
       </div>
+      <Header className={'hot-tip'} as={'h2'}>Hot tip: Maybe fire the barista?</Header>
     </div>
   )
 }
